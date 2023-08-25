39c31584a7491a75dccc2c8d39b73b9c755b00f3
Stephen Cooper <scooperdev@gmail.com>
2023-08-25 09:00:36 +0100

    Improve tests and local data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import { AgGridReact } from 'ag-grid-react';
-
+import data from './olympic-winners.json';
 const App = function (props) {
     const columnDefs = useMemo(() => [
         {
@@ -28,11 +28,15 @@ const App = function (props) {
 
     const gridRef = useRef(null);
     useEffect(() => {
-        fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+        setTimeout(() => {
+            setRowData(data)
+
+        }, 500)
+       /*  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
             .then((resp) => resp.json())
             .then((data) => {
-                setRowData(data);
-            });
+                setRowData();
+            }); */
     }, [])
 
     const handleSelectAll = () => {
@@ -45,10 +49,19 @@ const App = function (props) {
         setSelectedCount(gridRef.current.api.getSelectedRows().length);
     };
 
-    const handleRowClicked = (params) => {
-       // console.log('Clicked row',params.data);
+    const defaultRowClicked = (params) => {
+        // console.log('Clicked row',params.data);
+         setSelectedCount(gridRef.current.api.getSelectedRows().length);
+     };
+
+    const propsHandleRowClicked = props.handleRowClicked ? (params) => {
+       // console.log('Clicked row override',params.data);
+        props.handleRowClicked(params);
         setSelectedCount(gridRef.current.api.getSelectedRows().length);
-    };
+    }
+    :undefined;
+
+    const handleRowClicked = propsHandleRowClicked ?? defaultRowClicked;
     const handleCellClicked = (params) => {
     //    console.log('Clicked cell',params.value);
         setSelectedCellContents(params.value);
